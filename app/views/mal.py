f553ea9af7739ac5fa7b6cc31cb91dcd456d36ee
Cooper <kcoopermiller9@gmail.com>
2023-10-10 15:32:05 -0500

    fixed header bug

--- app/views/mal.py
+++ app/views/mal.py
@@ -5,7 +5,6 @@ import httpx
 bp = Blueprint('todo', __name__)
 
 URL = 'https://api.myanimelist.net/v2'
-HEADERS = {'Authorization': f'Bearer {request.headers["Authorization"]}'}
 
 # Get suggested anime for the authorized user
 # If the user is newcomer, this endpoint returns an empty list.
@@ -13,7 +12,7 @@ bp.get("/anime/suggestions/<int:limit>")
 async def suggest_anime(limit=10):
     print(request.headers)
     async with httpx.AsyncClient() as client:
-        response = await client.get(f'{URL}/anime/suggestions?limit={limit}', headers=HEADERS)
+        response = await client.get(f'{URL}/anime/suggestions?limit={limit}', headers={'Authorization': f'Bearer {request.headers["Authorization"]}'})
     
     return Response(response=json.dumps(response.json()), status=200)
 
@@ -21,7 +20,7 @@ async def suggest_anime(limit=10):
 bp.get("/anime/season/<int:year>/<string:season>/<int:limit>")
 async def anime_season(year, season, limit=10):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f'{URL}/anime/season/{year}/{season}?limit={limit}', headers=HEADERS)
+        response = await client.get(f'{URL}/anime/season/{year}/{season}?limit={limit}', headers={'Authorization': f'Bearer {request.headers["Authorization"]}'})
     
     return Response(response=json.dumps(response.json()), status=200)
     
