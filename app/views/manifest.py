d2d0ce6d53af4c7be692cd8c3b8e761b8062576c
Cooper <kcoopermiller9@gmail.com>
2023-10-10 00:41:20 -0500

    add json to static folder

--- app/views/manifest.py
+++ app/views/manifest.py
@@ -4,14 +4,14 @@ bp = Blueprint('manifest', __name__)
 
 @bp.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
-    with open("./.well-known/ai-plugin.json") as f:
+    with open("app/static/.well-known/ai-plugin.json") as f:
         text = f.read()
         return Response(text, mimetype="text/json")
 
 
 @bp.get("/openapi.yaml")
 async def openapi_spec():
-    with open("openapi.yaml") as f:
+    with open("app/openapi.yaml") as f:
         text = f.read()
         return Response(text, mimetype="text/yaml")
 
